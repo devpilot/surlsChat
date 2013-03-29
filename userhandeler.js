c1183ae8be46416449228a1bf66af5dcf33f974d
Aritra <aet419@gmail.com>
2013-03-30 03:07:52 +0530

    added module to handel users

---
+++ userhandeler.js
@@ -0,0 +1,47 @@
+function userHandeler(){
+
+	var userAvailable = [];
+
+	// storing available users
+	this.addUser = function(id){
+		userAvailable.push(id);
+	};
+
+	// remove partner from queue
+	var removePartner = function(id){
+		userAvailable.splice(id,1);
+	};
+
+	// remove self from queue
+	var removeSelf = function(id){
+		for(key in userAvailable){
+			if(userAvailable[key] === id)
+				userAvailable.splice(key,1);
+		}
+	};
+	
+	// Selecting random partner
+	var selectPartner = function(socketId, fn){
+		var queue = userAvailable;
+		if (queue.length > 1){
+			var key = Math.round(Math.random() * (queue.length - 1));
+			if(queue[key] == socketId){
+			  return selectPartner(socketId,fn);
+	    	} else {
+	    	   fn(queue[key],key);
+	    	}
+	    }
+	};
+	this.makeChat = function(socketId,fn){
+		selectPartner(socketId, function(partner,pid){
+			removePartner(pid);
+			removeSelf(socketId);
+			fn(partner);
+		});
+	};
+	this.leaveChat = function(socketId, pid){
+		userAvailable.push(pid);
+		userAvailable.push(socketId);
+	};
+}
+module.exports = new userHandeler;
